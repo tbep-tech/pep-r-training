979581588ee5b813e2b331bc7c0dec82a509bd1b
fawda123 <mbafs2012@gmail.com>
2021-02-16 07:41:42 -0500

    pepreporting to peptools, build maybe

--- rbasics.R
+++ rbasics.R
@@ -1,1 +1,84 @@
+# print
+print('hello world!')
 
+# sequence
+seq(1, 10)
+
+# random numbers
+rnorm(100, mean = 10, sd = 2)
+
+# average 
+mean(rnorm(100))
+
+# sum
+sum(rnorm(100))
+
+my_random_sum <- sum(rnorm(100))
+
+my_random_sum
+
+## # install packages from CRAN
+## install.packages("tidyverse")
+## install.packages("remotes")
+## install.packages("mapview")
+## remotes::install_github("remotes/peptools")
+
+library("tidyverse")
+library("peptools")
+
+## # Using the help command/shortcut
+## # When you know the name of a function
+## help("print") # Help on the print command
+## ?print # Help on the print command using the `?` shortcut
+## 
+## # When you know the name of the package
+## help(package = "sf") # Help on the package `dplyr`
+## 
+## # Don't know the exact name or just part of it
+## apropos("print") # Returns all available functions with "print" in the name
+## ??print # shortcut, but also searches demos and vignettes in a formatted page
+
+dbl_var <- c(1, 2.5, 4.5)
+int_var <- c(1L, 6L, 10L)
+log_var <- c(TRUE, FALSE, T, F)
+chr_var <- c("a", "b", "c")
+
+class(dbl_var)
+length(log_var)
+
+## # taking the mean of a character vector
+## mean(chr_var)
+## 
+## # adding two numeric vectors of different lengths
+## vec1 <- c(1, 2, 3, 4)
+## vec2 <- c(2, 3, 5)
+## vec1 + vec2
+
+ltrs <- c('a', 'b', 'c')
+nums <- c(1, 2, 3)
+logs <- c(T, F, T)
+mydf <- data.frame(ltrs, nums, logs)
+mydf
+
+library(readxl)
+rawdat <- read_excel('data/currentdata.xlsx')
+enterodat <- read_excel('data/enterodata.xlsx')
+
+# get the dimensions
+dim(rawdat)
+dim(enterodat)
+
+# get the column names
+names(rawdat)
+names(enterodat)
+
+# see the first six rows
+head(rawdat)
+head(enterodat)
+
+# get the overall structure
+str(rawdat)
+str(enterodat)
+
+## View(rawdat)
+## View(enterodat)
--- wq.R
+++ wq.R
@@ -1,1 +1,50 @@
+## library(peptools)
 
+pepstations
+
+mapview(pepseg)
+
+dat <- read_pepwq('data/currentdata.xlsx')
+head(dat)
+
+medpep <- anlz_medpep(dat)
+medpep
+
+anlz_attainpep(medpep)
+
+show_sitemappep(dat, 2019)
+
+show_sitemappep(dat, 2010, 'sd', c('Central', 'Eastern'))
+
+show_sitemappep(dat, 2019, relative = T)
+
+show_sitemappep(dat, 2019, relative = T, maxrel = 0.8)
+
+show_thrpep(dat, bay_segment = "Western", param = "chla")
+
+show_thrpep(dat, bay_segment = "Western", param = "sd")
+
+show_thrpep(dat, bay_segment = "Western", param = "chla", yrrng = c(1976, 2019))
+
+show_boxpep(dat, param = 'chla', bay_segment = "Western")
+show_boxpep(dat, param = 'sd', bay_segment = "Eastern")
+
+show_boxpep(dat, param = 'chla', bay_segment = "Western", yrrng = c(2008, 2018), yrsel = 1990)
+
+knitr::include_graphics('figure/outints.png')
+
+peptargets
+
+knitr::include_graphics('figure/matrixcats.png')
+
+show_matrixpep(dat)
+
+show_matrixpep(dat, asreact = TRUE)
+
+show_wqmatrixpep(dat)
+
+show_wqmatrixpep(dat, param = 'sd')
+
+show_segmatrixpep(dat, bay_segment = 'Western')
+
+show_plotlypep(rawdat)
